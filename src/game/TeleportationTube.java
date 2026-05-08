52ef8e639abe382dfe17e22fdd57623dc65d1c80
Chin Heng Li <hchi0056@student.monash.edu>
2026-05-08 18:27:15 +0800

    Add TeleportationTube malfunction and fire effects

---
+++ src/game/TeleportationTube.java
@@ -0,0 +1,132 @@
+package game;
+
+import game.actions.Teleportable;
+import game.actions.TeleportAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * A teleportation tube that allows actors to teleport to pre-set destinations.
+ * Has a 50% malfunction rate that causes random teleportation within the destination map.
+ * Sets adjacent tiles on fire at the destination.
+ */
+public class TeleportationTube extends Ground implements Teleportable {
+    private List<Location> destinations = new ArrayList<>();
+    private Random random = new Random();
+
+    /**
+     * Constructor for TeleportationTube.
+     */
+    public TeleportationTube() {
+        super('Φ', "Teleportation Tube");
+    }
+
+    /**
+     * Add a destination to this tube's destination list.
+     * @param destination the destination location
+     */
+    public void addDestination(Location destination) {
+        destinations.add(destination);
+    }
+
+    /**
+     * Get the list of possible destinations.
+     * @param map the current game map
+     * @return the list of pre-set destinations
+     */
+    @Override
+    public List<Location> getDestinations(GameMap map) {
+        return new ArrayList<>(destinations);
+    }
+
+    /**
+     * Execute teleportation with 50% malfunction chance.
+     * Sets adjacent tiles on fire at destination and applies burn status to actor.
+     * @param actor the actor being teleported
+     * @param source the source location
+     * @param destination the destination location (may be overridden by malfunction)
+     * @param map the current game map
+     */
+    @Override
+    public void onTeleport(Actor actor, Location source, Location destination, GameMap map) {
+        Location finalDestination = destination;
+
+        // 50% chance of malfunction
+        if (random.nextDouble() < 0.5) {
+            // Random destination within the destination map
+            List<Location> randomLocs = getAllLocationInMap(destination.map);
+            if (!randomLocs.isEmpty()) {
+                finalDestination = randomLocs.get(random.nextInt(randomLocs.size()));
+            }
+        }
+
+        // Set adjacent tiles on fire
+        setAdjacentTilesOnFire(finalDestination);
+
+        // Apply burn status to actor
+        actor.addCapability(new BurnStatus(2, 1));
+    }
+
+    /**
+     * Get all actions available for an actor at this location.
+     * @param actor the actor
+     * @param location the location of this ground
+     * @return action list containing TeleportAction
+     */
+    @Override
+    public ActionList allowableActions(Actor actor, Location location) {
+        ActionList actions = new ActionList();
+        actions.add(new TeleportAction(this));
+        return actions;
+    }
+
+    /**
+     * Actors can enter the teleportation tube.
+     * @param actor the actor
+     * @return true
+     */
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        return true;
+    }
+
+    /**
+     * Set all adjacent floor tiles on fire.
+     * @param location the location whose adjacent tiles to set on fire
+     */
+    private void setAdjacentTilesOnFire(Location location) {
+        for (Exit exit : location.getExits()) {
+            Location adjacent = exit.getDestination();
+            if (adjacent.getGround() instanceof Floor) {
+                adjacent.setGround(new Fire(2));
+            }
+        }
+    }
+
+    /**
+     * Get all locations in a map for random destination selection.
+     * @param map the game map
+     * @return list of all locations in the map
+     */
+    private List<Location> getAllLocationInMap(GameMap map) {
+        List<Location> allLocations = new ArrayList<>();
+        // Get width and height from map
+        for (int x = 0; x < map.getWidth(); x++) {
+            for (int y = 0; y < map.getHeight(); y++) {
+                Location loc = map.at(x, y);
+                if (loc != null && loc.canActorEnter(null)) {
+                    allLocations.add(loc);
+                }
+            }
+        }
+        return allLocations;
+    }
+}
