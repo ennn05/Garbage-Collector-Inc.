bf0bb9152fb740b84827d6212b90b90daad4079a
Yee Yang Siow <ysio0005@student.monash.edu>
2026-05-12 16:27:59 +0800

    Feat: Hole expansion and spawning updates - Implement new spawn logic for more robust structure. - Implement new space detection logic for potential hole expansion to adjacent tiles.

--- src/game/grounds/Hole.java
+++ src/game/grounds/Hole.java
@@ -4,16 +4,24 @@ import edu.monash.fit2099.engine.GameEngineException;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
+import game.actors.Parasite;
 import game.actors.Slime;
 import game.actors.Undead;
+import game.interfaces.Spawner;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 /**
  * A hole that periodically spawns moon creatures.
  */
-public class Hole extends Ground {
+public class Hole extends Ground implements Spawner {
     private static final int SPAWN_INTERVAL = 20;
+    private static final double GROW_CHANCE = 0.01;
+    private static final int SPAWN_CHECK_RADIUS = 1;
+
     private final Random random = new Random();
     private int turnsElapsed;
 
@@ -24,6 +32,7 @@ public class Hole extends Ground {
 
     /**
      * Count turns and attempt to spawn exactly once every 20 turns.
+     * There is a 1% chance where the Hole expands to an adjacent ground.
      *
      * @param location the location of this ground
      */
@@ -35,22 +44,39 @@ public class Hole extends Ground {
             return;   // no generate
         }
 
-        Actor spawnedActor;    // spawn actor save
-
-        if (random.nextBoolean()) {     // 50%
-            spawnedActor = new Undead();
-        } else {
-            spawnedActor = new Slime();
-        }
-
         try {
             if (!location.containsAnActor()) { // empty then generate
-                location.addActor(spawnedActor);
+                location.addActor(this.spawn(location));
+                if (random.nextDouble() < GROW_CHANCE) {
+                    List<Location> adj = new ArrayList<>(location.getNearbyLocations(SPAWN_CHECK_RADIUS));
+                    Collections.shuffle(adj); // shuffle for random adjacent
+                    for (Location nearby : adj) {
+                        if (nearby.getGroundAs(Hole.class) == null) {
+                            nearby.setGround(new Hole());
+                            break;
+                        }
+                    }
+                }
             }
         } catch (GameEngineException e) {
-            // fail then skip
+            throw new RuntimeException(e);
         }
 
         turnsElapsed = 0; // 20rounds attempt happened
+    }
+
+    @Override
+    public Actor spawn(Location location) {
+        if (random.nextBoolean()) {
+            return Undead.getUndeadSpawn();
+        } else {
+            if (location.map().toString().equals("99-Deprecated")) {
+                return Slime.getSlimeSpawn();
+            } else if (location.map().toString().equals("20-Overflow")) {
+                return Parasite.getParasiteSpawn();
+            } else {
+                return Undead.getUndeadSpawn(); // failsafe spawn Undead if map is not recognised
+            }
+        }
     }
 }
