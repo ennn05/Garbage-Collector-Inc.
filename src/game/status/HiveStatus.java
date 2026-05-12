60f6530455c419ab256f2cc4a0dbc25c6a463b12
Yee Yang Siow <ysio0005@student.monash.edu>
2026-05-12 11:10:07 +0800

    Feat: new HiveStatus for handling hive infection instance - Implement HiveStatus as counter for spawning at intervals once infected and became a Host.

---
+++ src/game/status/HiveStatus.java
@@ -0,0 +1,82 @@
+package game.status;
+
+import edu.monash.fit2099.engine.GameEngineException;
+import edu.monash.fit2099.engine.GameEntity;
+import edu.monash.fit2099.engine.capabilities.Status;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.Location;
+import game.interfaces.Host;
+import game.interfaces.Spawnable;
+
+import java.util.Optional;
+
+/**
+ * A status effect that causes a host to periodically spawn a new entity into an adjacent empty location.
+ * <p>
+ * Each tick, the host's spawn effect is triggered. Once the spawn counter reaches zero,
+ * the configured {@link Spawnable} is spawned into the first available adjacent location.
+ */
+public class HiveStatus implements Status {
+    private final Spawnable spawnable;
+    private final int spawnInterval;
+    private int spawnCounter;
+
+    /**
+     * Creates a hive status that spawns the supplied entity at the given interval.
+     *
+     * @param spawnable the entity factory used when spawning a new entity
+     * @param spawnInterval the number of ticks between spawn attempts
+     */
+    public HiveStatus(Spawnable spawnable, int spawnInterval) {
+        this.spawnable = spawnable;
+        this.spawnInterval = spawnInterval;
+        this.spawnCounter = spawnInterval;
+    }
+
+    /**
+     * Advances this status by one tick.
+     * <p>
+     * If the current entity has the {@link Host} capability, its spawn effect is triggered.
+     * When the internal counter reaches zero, this status attempts to spawn a new entity
+     * into the first adjacent location that does not already contain an actor.
+     *
+     * @param currEntity the entity currently carrying this status
+     * @param location the location of the current entity
+     */
+    @Override
+    public void tickStatus(GameEntity currEntity, Location location) {
+        Optional<Host> host = currEntity.asCapability(Host.class);
+        if (host.isPresent()) {
+            Host hostEntity = host.get();
+            hostEntity.spawnEffect();
+            if (spawnCounter <= 0) {
+                boolean spawned = false;
+                for (Exit exit : location.getExits()) {
+                    Location destination = exit.getDestination();
+                    if (!destination.containsAnActor()) {
+                        try {
+                            destination.addActor(spawnable.spawn());
+                            spawned = true;
+                            break;
+                        } catch (GameEngineException e) {
+                            System.out.println("Failed to spawn entity: " + e.getMessage());
+                        }
+                    }
+                }
+                if (spawned) spawnCounter = spawnInterval;
+            } else {
+                spawnCounter--;
+            }
+        }
+    }
+
+    /**
+     * Hive status is always active while attached to an entity.
+     *
+     * @return {@code true}
+     */
+    @Override
+    public boolean isStatusActive() {
+        return true;
+    }
+}
