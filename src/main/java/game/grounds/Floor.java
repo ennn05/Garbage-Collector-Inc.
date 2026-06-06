fc756b56ff78cc88f56a5b431e93511c8ad0e2d5
Yee Yang Siow <ysio0005@student.monash.edu>
2026-06-06 16:42:50 +0800

    Feat: add chance based degradation for Floor - Implement a chance based (set to 50%) degradation rate for any shockwave instance.

--- src/main/java/game/grounds/Floor.java
+++ src/main/java/game/grounds/Floor.java
@@ -1,8 +1,11 @@
 package game.grounds;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
 import game.interfaces.Seedable;
+
+import java.util.Random;
 
 /**
  * Not lava. Not spikes. Not an elaborate trap. Just a perfectly flat surface
@@ -12,7 +15,10 @@ import game.interfaces.Seedable;
  * @author Adrian Kristanto
  */
 public class Floor extends CrackableGround implements Seedable {
+    private static final int DEGRADATION_CHANCE = 50;
     private static final int DEGRADATION_LEVEL = 0;
+
+    private final Random rand = new Random();
 
     /**
      * Creates a Floor tile (display character: {@code _}).
@@ -26,7 +32,10 @@ public class Floor extends CrackableGround implements Seedable {
     public void onActorStep(Location location, Actor actor) {}
 
     @Override
-    public CrackedFloor degrade() {
+    public Ground degrade() {
+        if (rand.nextInt(100) < DEGRADATION_CHANCE) {
+            return this;
+        }
         return new CrackedFloor();
     }
 }
