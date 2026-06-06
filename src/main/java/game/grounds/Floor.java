5e01809f05d3894d186a0b53f36c4a98adc1e68e
Yee Yang Siow <ysio0005@student.monash.edu>
2026-06-06 14:26:50 +0800

    Feat: add CrackableGround abstraction to Floor - Floor to extend CrackableGround for cracking logic. - Turns into CrackedFloor if degraded. - Update CrackableGround abstractions.

--- src/main/java/game/grounds/Floor.java
+++ src/main/java/game/grounds/Floor.java
@@ -1,6 +1,8 @@
 package game.grounds;
 
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
 import game.interfaces.Seedable;
 
 /**
@@ -10,12 +12,22 @@ import game.interfaces.Seedable;
  *
  * @author Adrian Kristanto
  */
-public class Floor extends Ground implements Seedable {
+public class Floor extends CrackableGround implements Seedable {
+    private static final int DEGRADATION_LEVEL = 0;
+
     /**
      * Creates a Floor tile (display character: {@code _}).
      * Implements {@link game.interfaces.Seedable} so fungal ground can spread here.
      */
     public Floor() {
-        super('_', "Floor");
+        super('_', "Floor", DEGRADATION_LEVEL);
+    }
+
+    @Override
+    public void onActorStep(Location location, Actor actor) {}
+
+    @Override
+    public CrackedFloor degrade() {
+        return new CrackedFloor();
     }
 }
