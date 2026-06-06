6ad3257f19ebbb5adf2e6f3c2535bf79a9138347
Yee Yang Siow <ysio0005@student.monash.edu>
2026-06-06 22:02:27 +0800

    Refactor: introduce ItemStatistic and use of allowableActions() - ItemStatistic to handle durability and weight statistics. - Use allowableActions method to handle instantiating Actions.

--- src/main/java/game/items/BeaconDetonator.java
+++ src/main/java/game/items/BeaconDetonator.java
@@ -1,16 +1,25 @@
 package game.items;
 
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.statistics.BaseStatistic;
+import game.actions.DetonateAction;
+import game.enums.ItemStatistics;
 
 public class BeaconDetonator extends Item {
     private static final int DETONATE_RANGE = 5;
+    private static final int WEIGHT = 1;
 
     public BeaconDetonator() {
         super("Remote Detonator", '[');
         this.makePortable();
+        this.addNewStatistic(ItemStatistics.WEIGHT, new BaseStatistic(WEIGHT));
     }
 
-    public static int getDetonateRange() {
-        return DETONATE_RANGE;
+    @Override
+    public ActionList allowableActions(Actor owner, GameMap map) {
+        return new ActionList(new DetonateAction(DETONATE_RANGE));
     }
 }
