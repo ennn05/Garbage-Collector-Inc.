d16f441a3a2994930933d4ba5b9a32995089110e
Yee Yang Siow <ysio0005@student.monash.edu>
2026-06-07 03:36:35 +0800

    Feat: add all resonator items to be Purchasable from Supercomputer - BeaconDetonator can be purchased from Supercomputer - QuakeCharge can be purchased from Supercomputer - TremorBeacon can be purchased from Supercomputer.

--- src/game/items/BeaconDetonator.java
+++ src/game/items/BeaconDetonator.java
@@ -4,12 +4,16 @@ import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.statistics.BaseStatistic;
 import game.actions.DetonateAction;
+import game.economy.Wallet;
 import game.enums.ItemStatistics;
+import game.interfaces.Purchasable;
 
-public class BeaconDetonator extends Item {
+public class BeaconDetonator extends Item implements Purchasable {
     private static final int DETONATE_RANGE = 5;
+    private static final int PURCHASE_PRICE = 15;
     private static final int WEIGHT = 1;
 
     public BeaconDetonator() {
@@ -21,5 +25,34 @@ public class BeaconDetonator extends Item {
     @Override
     public ActionList allowableActions(Actor owner, GameMap map) {
         return new ActionList(new DetonateAction(DETONATE_RANGE));
+    }
+
+    @Override
+    public BeaconDetonator createPurchasedItem() {
+        return new BeaconDetonator();
+    }
+
+    @Override
+    public int getPurchasePrice() {
+        return PURCHASE_PRICE;
+    }
+
+    /**
+     * Handle insufficient credits response.
+     *
+     * @param buyer the actor attempting to buy this item
+     * @param map the map where the transaction happens
+     * @return result description
+     */
+    @Override
+    public String onInsufficientCredits(Actor buyer, GameMap map) {
+        String message = buyer + " cannot afford the Beacon Detonator (costs " + PURCHASE_PRICE + " credits).";
+        System.out.println(message);
+        return message;
+    }
+
+    @Override
+    public String onPurchased(Actor buyer, GameMap map, Location terminalLocation, Wallet wallet) {
+        return buyer + " purchased the Beacon Detonator";
     }
 }
